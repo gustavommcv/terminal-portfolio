1cb35512df964a729a6c758df49dbaf86937d71d
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-07-22 13:35:48 -0300

    Tech stack section

---
+++ src/app/features/home/components/tech-stack-section/tech-stack-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TerminalSection } from '../../../../core/layout/terminal-section/terminal-section';
+
+@Component({
+  selector: 'tech-stack-section',
+  imports: [TerminalSection],
+  templateUrl: './tech-stack-section.html',
+  styleUrl: './tech-stack-section.scss',
+})
+export class TechStackSection {}
--- src/app/features/home/home-page/home-page.ts
+++ src/app/features/home/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PresentationSection } from '../components/presentation-section/presentation-section';
 import { ServicesSection } from '../components/services-section/services-section';
+import { TechStackSection } from '../components/tech-stack-section/tech-stack-section';
 
 @Component({
   selector: 'home-page',
-  imports: [PresentationSection, ServicesSection],
+  imports: [PresentationSection, ServicesSection, TechStackSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
