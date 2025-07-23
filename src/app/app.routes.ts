acd565f573e7cd9afb718d64d1a6ab60e7df0f10
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-07-22 22:47:17 -0300

    button navigation v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomePage } from './features/home/home-page/home-page';
 import { ErrorPage } from './features/error/error-page/error-page';
 import { AboutPage } from './features/about/about-page/about-page';
+import { PortfolioPage } from './features/portfolio/portfolio-page/portfolio-page';
 
+// TODO
+// Lazy
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'about', component: AboutPage },
+  { path: 'portfolio', component: PortfolioPage },
   { path: '**', component: ErrorPage },
 ];
--- src/app/core/shared/app-button/app-button.ts
+++ src/app/core/shared/app-button/app-button.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LanguageService } from '../../../services/language.service';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app-button.html',
   styleUrl: './app-button.scss',
 })
-export class AppButton {}
+export class AppButton {
+  @Input() route: string = '';
+  @Input() scrollTo: string = '';
+
+  constructor(
+    private languageService: LanguageService,
+    private sanitizer: DomSanitizer,
+  ) { }
+
+  get safeHref(): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(this.route || '#');
+  }
+
+  handleClick(event: Event) {
+    if (this.scrollTo) {
+      event.preventDefault();
+      this.scrollToElement(this.scrollTo);
+    } else if (this.route) {
+      event.preventDefault();
+      this.languageService.navigateWithLocale(this.route);
+    }
+  }
+
+  private scrollToElement(elementId: string) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
