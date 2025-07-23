35e85a533d6dc88d78f2ae44b4a29212d149e434
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-07-22 22:46:33 -0300

    button navigation

---
+++ src/app/features/portfolio/portfolio-page/portfolio-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-page',
+  imports: [],
+  templateUrl: './portfolio-page.html',
+  styleUrl: './portfolio-page.scss',
+})
+export class PortfolioPage {}
