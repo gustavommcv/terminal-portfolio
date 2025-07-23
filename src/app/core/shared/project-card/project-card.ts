1b5cfc376e26030d2c551533b75ff40d2e75c295
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-07-23 15:59:54 -0300

    project card as link

--- src/app/core/shared/project-card/project-card.ts
+++ src/app/core/shared/project-card/project-card.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../../data/projects.data';
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../../services/language.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'project-card',
-  imports: [TranslateModule],
+  standalone: true,
+  imports: [TranslateModule, CommonModule],
   templateUrl: './project-card.html',
   styleUrl: './project-card.scss',
 })
 export class ProjectCard {
   @Input() project!: Project;
+
+  constructor(public language: LanguageService) {}
+
+  navigateToDetail() {
+    this.language.navigateWithLocale('/projects/' + this.project.id);
+  }
 }
