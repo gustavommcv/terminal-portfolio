0ab469f1dd9afaaff825cdb64161634d43fe1608
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-07-24 13:10:57 -0300

    Client + Pre render routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'portfolio',
+    renderMode: RenderMode.Prerender,
+  },
+
+  {
+    path: 'projects/:id',
+    renderMode: RenderMode.Client,
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
