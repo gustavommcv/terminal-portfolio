6bfeb40c0bf9683a7428fc0a1c88dd79c9558ab7
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-07-24 14:35:52 -0300

    footer

--- src/app/features/home/home-page/home-page.ts
+++ src/app/features/home/home-page/home-page.ts
@@ -4,10 +4,11 @@ import { ServicesSection } from '../components/services-section/services-section
 import { TechStackSection } from '../components/tech-stack-section/tech-stack-section';
 import { ContactSection } from '../components/contact-section/contact-section';
 import { FeaturedProjectsSection } from '../components/featured-projects-section/featured-projects-section';
+import { Footer } from '../../../core/layout/footer/footer';
 
 @Component({
   selector: 'home-page',
-  imports: [PresentationSection, ServicesSection, TechStackSection, ContactSection, FeaturedProjectsSection],
+  imports: [PresentationSection, ServicesSection, TechStackSection, ContactSection, FeaturedProjectsSection, Footer],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
